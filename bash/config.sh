4d6079d419e9c95e86728abaa4d1bcd57582fb31
Flavia Fortes <flaviafortes88@gmail.com>
2015-05-07 12:49:26 -0300

    Improve git-prompt

--- bash/config.sh
+++ bash/config.sh
@@ -1,6 +1,13 @@
 # Editor
 export EDITOR="subl -w"
 
+export GIT_PS1_SHOWDIRTYSTATE=true
+export GIT_PS1_SHOWSTASHSTATE=true
+export GIT_PS1_SHOWUNTRACKEDFILES=true
+export GIT_PS1_SHOWCOLORHINTS=true
+export GIT_PS1_DESCRIBE_STYLE=branch
+export GIT_PS1_SHOWUPSTREAM=verbose
+
 # Customize the terminal input line
 prompt() {
   PS1="${CYAN}\w ${MAGENTA}\$(__git_ps1)\n${RED} ${NORMAL}"
